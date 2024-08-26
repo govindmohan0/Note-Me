309c0c16b0d078493aaeae3ddcda6cfa27329a5b
Govind Mohan Shah <www.govind020@gmail.com>
2024-08-26 10:52:11 +0530

    ADDED OS PDF

--- client/src/components/Resources/OS.jsx
+++ client/src/components/Resources/OS.jsx
@@ -4,16 +4,16 @@ import Sidebar from "../Sidebar";
 
 const OS = () => {
     const [pdfUrl, setPdfUrl] = useState(
-        "https://www.antennahouse.com/hubfs/xsl-fo-sample/pdf/basic-link-1.pdf"
+        "https://raw.githubusercontent.com/govindmohan0/Note-me-Notes/main/OS/1%20UNIT%20I.pdf"
     );
 
     // Define options specific to Operating Systems
     const OSOptions = [
-        { name: 'Introduction to Operating Systems', pdfUrl: 'https://github.com/govindmohan0/Note-me-Notes/blob/main/OS/1%20UNIT%20I.pdf' },
-        { name: 'Processes and Threads', pdfUrl: 'https://github.com/govindmohan0/Note-me-Notes/blob/main/OS/2%20UNIT%20II.pdf' },
-        { name: 'Memory Management', pdfUrl: 'https://github.com/govindmohan0/Note-me-Notes/blob/main/OS/3%20UNIT%20III.pdf' },
-        { name: 'File Systems', pdfUrl: 'https://github.com/govindmohan0/Note-me-Notes/blob/main/OS/4%20UNIT%20IV.pdf' },
-        { name: 'Deadlocks and Disk Management', pdfUrl: 'https://github.com/govindmohan0/Note-me-Notes/blob/main/OS/5%20UNIT%20V.pdf' },
+        { name: 'Introduction to Operating Systems', pdfUrl: 'https://raw.githubusercontent.com/govindmohan0/Note-me-Notes/main/OS/1%20UNIT%20I.pdf' },
+        { name: 'Processes and Threads', pdfUrl: 'https://raw.githubusercontent.com/govindmohan0/Note-me-Notes/main/OS/2%20UNIT%20II.pdf' },
+        { name: 'Memory Management', pdfUrl: 'https://raw.githubusercontent.com/govindmohan0/Note-me-Notes/main/OS/3%20UNIT%20III.pdf' },
+        { name: 'File Systems', pdfUrl: 'https://raw.githubusercontent.com/govindmohan0/Note-me-Notes/main/OS/4%20UNIT%20IV.pdf' },
+        { name: 'Deadlocks and Disk Management', pdfUrl: 'https://raw.githubusercontent.com/govindmohan0/Note-me-Notes/main/OS/5%20UNIT%20V.pdf' },
       
         // Add more options as needed
     ];
